d9ecf360a670e3dbb2697e6dc2b940bfe5ccbfb1
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-30 06:18:09 +0600

    added vercel insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
